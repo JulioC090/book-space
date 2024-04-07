405662f1b77890ab82a20a04bc61c91293a450ab
JulioC090 <juliocziviani@gmail.com>
2024-04-07 02:50:58 -0300

    refac(web): extract AuthFormTemplate

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -1,25 +1,19 @@
 'use client';
 
-import Center from '@/components/atoms/Center';
-import LinkButton from '@/components/atoms/LinkButton';
 import LoginForm from '@/components/organism/LoginForm';
-import Image from 'next/image';
+import AuthFormTemplate from '@/components/templates/AuthFormTemplate';
 
 export default function LoginPage() {
   return (
-    <Center>
-      <Image
-        width={256}
-        height={256}
-        src="/book-space-logo.png"
-        alt="Book Space Logo"
-      />
-      <h1 className="text-2xl font-bold mb-12">Entrar no Book Space</h1>
+    <AuthFormTemplate
+      title="Entrar no Book Space"
+      link={{
+        message: 'Você não possui uma conta? ',
+        href: '/signup',
+        children: 'Cadastrar-se',
+      }}
+    >
       <LoginForm />
-      <p className="text-sm mt-4">
-        Você não possui uma conta?{' '}
-        <LinkButton href={'/signup'}>Cadastrar-se</LinkButton>
-      </p>
-    </Center>
+    </AuthFormTemplate>
   );
 }
--- web/src/app/(auth)/signup/page.tsx
+++ web/src/app/(auth)/signup/page.tsx
@@ -1,23 +1,17 @@
-import Center from '@/components/atoms/Center';
-import LinkButton from '@/components/atoms/LinkButton';
 import SignUpForm from '@/components/organism/SignUpForm';
-import Image from 'next/image';
+import AuthFormTemplate from '@/components/templates/AuthFormTemplate';
 
 export default function SignUpPage() {
   return (
-    <Center>
-      <Image
-        width={256}
-        height={256}
-        src="/book-space-logo.png"
-        alt="Book Space Logo"
-      />
-      <h1 className="text-2xl font-bold mb-12">Cadastre-se no Book Space</h1>
+    <AuthFormTemplate
+      title="Cadastre-se no Book Space"
+      link={{
+        message: 'Você já possui uma conta?',
+        href: '/login',
+        children: 'Entrar',
+      }}
+    >
       <SignUpForm />
-      <p className="text-sm mt-4">
-        Você já possui uma conta?{' '}
-        <LinkButton href={'/login'}>Entrar</LinkButton>
-      </p>
-    </Center>
+    </AuthFormTemplate>
   );
 }
