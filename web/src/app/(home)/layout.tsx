0c4fb221df01289d5e5db2e2024134dd3fb15578
JulioC090 <juliocziviani@gmail.com>
2024-04-07 16:15:11 -0300

    refac(web): extract NavBar

--- web/src/app/(home)/layout.tsx
+++ web/src/app/(home)/layout.tsx
@@ -1,44 +1,15 @@
 'use client';
 
-import { SideMenu } from '@/components/molecules/SideMenu';
-import SideMenuActionItem from '@/components/molecules/SideMenu/SideMenuActionItem';
-import { AuthContext } from '@/contexts/AuthContext';
-import { BookOpen, HouseSimple, SignOut } from '@phosphor-icons/react/dist/ssr';
-import { useContext } from 'react';
+import NavBar from '@/components/organism/Navbar';
 
 export interface SideMenuLayoutProps {
   children: React.ReactNode;
 }
 
 export default function SideMenuLayout({ children }: SideMenuLayoutProps) {
-  const { userInfo, logout } = useContext(AuthContext);
-
-  if (!userInfo) return;
-
   return (
     <div className="flex flex-col-reverse h-screen sm:h-fit sm:flex-row w-full">
-      <SideMenu.Root>
-        <SideMenu.Header user={userInfo!} />
-        <SideMenu.List>
-          <SideMenu.LinkItem
-            icon={<HouseSimple />}
-            link="/"
-            label="Workspaces"
-          />
-          <SideMenu.LinkItem
-            icon={<BookOpen />}
-            link="/book"
-            label="Agendar espaço"
-          />
-        </SideMenu.List>
-        <SideMenu.Footer>
-          <SideMenuActionItem
-            onClick={logout}
-            icon={<SignOut />}
-            label="Sair"
-          />
-        </SideMenu.Footer>
-      </SideMenu.Root>
+      <NavBar />
       <main className="flex-grow sm:min-h-screen overflow-y-auto">
         {children}
       </main>
--- web/src/components/molecules/SideMenu/SideMenuHeader.tsx
+++ web/src/components/molecules/SideMenu/SideMenuHeader.tsx
@@ -7,11 +7,11 @@ export interface SideMenuHeaderProps {
 
 export default function SideMenuHeader({ user }: SideMenuHeaderProps) {
   return (
-    <header className="sm:flex gap-4 items-center hidden">
+    <header className="sm:flex gap-4 justify-center hidden">
       <div className="flex justify-center items-center rounded-full p-3 size-9 bg-zinc-500 lg:p-4 lg:size-12">
         {getInitials(user.name, 1)}
       </div>
-      <div className="hidden lg:block">
+      <div className="hidden lg:block lg:flex-grow">
         <p className="font-semibold">{user.name}</p>
         <span className="text-xs text-zinc-400">{user.email}</span>
       </div>
